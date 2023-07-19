0d4e1b016975e934ebcb50c0d0e965597bdb9432
Silvana A <alfonsosilvana16@gmail.com>
2023-07-19 14:03:30 -0300

    validación form_consultas

--- controllers/productos-controllers.js
+++ controllers/productos-controllers.js
@@ -47,3 +47,77 @@ verConsolasBtn.addEventListener("click", () => {
   diversosSection.style.display = "none";
   consolasSection.style.display = "block";
 });
+
+/* Validacion form_consultas */
+
+const formConsultas = document.querySelector('.form_consultas');
+const inputNombre = formConsultas.querySelector('input[type="text"]');
+const textareaMensaje = formConsultas.querySelector('textarea');
+const formBtn = formConsultas.querySelector('.form_btn');
+
+const nombreError = createErrorMessage('Este es requerido y debe tener máximo 40 caracteres.');
+const mensajeError = createErrorMessage('Este campo es requerido y debe tener máximo 120 caracteres.');
+const successMessage = document.querySelector('.success-message');
+
+inputNombre.after(nombreError);
+textareaMensaje.after(mensajeError);
+
+formBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const nombre = inputNombre.value.trim();
+  const mensaje = textareaMensaje.value.trim();
+  let isValid = true;
+
+  isValid = validateField(nombre, nombreError, [
+    { condition: !nombre, message: 'El campo de nombre es requerido y debe tener máx 40 caracteres.'},
+    { condition: nombre.length > 40}
+  ]);
+
+  isValid = validateField(mensaje, mensajeError, [
+    { condition: !mensaje, message: 'El campo de mensaje es requerido y debe tener máx 120 caracteres.' },
+    { condition: mensaje.length > 120 }
+  ]) && isValid;
+
+  if (isValid) {
+    showSuccess(successMessage);
+    console.log('Mensaje enviado:', nombre, mensaje);
+    formConsultas.reset();
+  }
+});
+
+function createErrorMessage(text) {
+  const errorMessage = document.createElement('span');
+  errorMessage.classList.add('error-message');
+  errorMessage.textContent = text;
+  errorMessage.style.display = 'none';
+  return errorMessage;
+}
+function createSuccessMessage(text) {
+  const successMessage = document.createElement('span');
+  successMessage.classList.add('success-message');
+  successMessage.textContent = text;
+  successMessage.style.display = 'none';
+  return successMessage;
+}
+function validateField(value, errorElement, validations) {
+  let isValid = true;
+  errorElement.style.display = 'none';
+
+  validations.forEach(({ condition, message }) => {
+    if (condition) {
+      showError(errorElement, message);
+      isValid = false;
+    }
+  });
+  return isValid;
+}
+function showError(element, text) {
+  element.textContent = text || element.textContent;
+  element.style.display = 'block';
+}
+function showSuccess(element) {
+  element.style.display = 'block';
+  setTimeout(() => {
+    element.style.display = 'none';
+  }, 10000);
+}
